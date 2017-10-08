1165289a45ed13f2437ec995df1134a45fed0e3c
URANO Ryoya <urano.works.mail@gmail.com>
2017-10-08 21:14:03 +0900

    アカウント用のテーブル

--- data/enquete.sql
+++ data/enquete.sql
@@ -1,21 +1,28 @@
+CREATE TABLE user (
+    id          INTEGER PRIMARY KEY,
+    name        TEXT NOT NULL,
+    icon        BLOB,
+    access_token    TEXT NOT NULL
+);
+
 CREATE TABLE enquete (
     id          INTEGER PRIMARY KEY,
-    author      VARCHAR(20) NOT NULL,
     title       VARCHAR(100) NOT NULL,
+    user        VARCHAR(20) NOT NULL,
     description TEXT NOT NULL
 );
 
 CREATE TABLE answer (
     id          INTEGER PRIMARY KEY,
-    author      VARCHAR(20) NOT NULL,
-    answer       TEXT NOT NULL,
+    user        VARCHAR(20) NOT NULL,
+    answer      TEXT NOT NULL,
     enquete_id  INTEGER NOT NULL,
     FOREIGN KEY(enquete_id) REFERENCES enquete(id)
 );
 
 CREATE TABLE comment (
     id          INTEGER PRIMARY KEY,
-    author      VARCHAR(20) NOT NULL,
+    user        VARCHAR(20) NOT NULL,
     comment     TEXT NOT NULL,
     answer_id   INTEGER NOT NULL,
     FOREIGN KEY(answer_id) REFERENCES answer(id)
