1cb023929130e6876d79e0b00951274936461091
URANO Ryoya <urano.works.mail@gmail.com>
2017-10-12 14:21:47 +0900

    :construction: JSONの取得とパース

--- app/controllers/AuthorizeController.scala
+++ app/controllers/AuthorizeController.scala
@@ -5,16 +5,11 @@ import javax.inject._
 import play.api.mvc._
 import play.api.db._
 import play.api.libs.ws.WSClient
-import play.api.libs.json.Json
+import play.api.libs.json._
 import dispatch._
 import Defaults._
-import org.asynchttpclient.HttpResponseBodyPart
+import com.fasterxml.jackson.annotation.JsonValue
 
-case class AccessToken(access_token: String, scope: String, token_type: String)
-object AccessToken {
-    implicit def jsonWrites = Json.writes[AccessToken]
-    implicit def jsonReads = Json.reads[AccessToken]
-}
 
 @Singleton
 class AuthorizeController @Inject()(db: Database)(ws: WSClient) extends Controller {
@@ -33,16 +28,29 @@ class AuthorizeController @Inject()(db: Database)(ws: WSClient) extends Controll
         // 3. GitHubからは、コールバックしてくるURLにクエリとしてアクセスコードが含められる。
         // 4. アクセスコード、client_id、client_secretを含めてリクエストをすると、access_tokenが得られる。
         val sessionCode = request.getQueryString("code").getOrElse("")
-        println(sessionCode)
-        val conn = url("https://github.com/login/oauth/access_token").POST
+        val accessToken: String = {
+            val conn = url("https://github.com/login/oauth/access_token").POST
             .addHeader("Accept", "application/json")
             .addParameter("client_id", clientId)
             .addParameter("client_secret", clientSecret)
             .addParameter("code", sessionCode)
-        // TODO: エラー処理を書いていないので追加する。
-        val accessToken = Http.default(conn OK as.String)
-        val result = accessToken()
-        val accessTokenJson = Json.fromJson[AccessToken](Json.parse(result)).get
+            // TODO: エラー処理を書いていないので追加する。
+            val result = Http.default(conn OK as.String)
+            (Json.parse(result()) \ "access_token").as[String] // as[T]による変換は安全ではない(今は実験
+        }
+
+        // Userデータを取得する
+        val userJson: JsValue = {
+            val conn = url("https://api.github.com/user")
+                .addHeader("Authorization", "token " + accessToken)
+            val result = Http.default(conn OK as.String)
+            Json.parse(result())
+        }
+        println(userJson)
+        val githubId = (userJson \ "id").asOpt[Int]
+        val githubName = (userJson \ "name").asOpt[String]
+        val githubIconUrl = (userJson \ "avatar_url").asOpt[String]
+        println("id: " + githubId + "\nname: " + githubName + "\nicon" + githubIconUrl)
         // これで一応JSONをパースできるが、レスポンスがエラーの場合死ぬ
         Redirect(routes.EnqueteController.index())
     }
