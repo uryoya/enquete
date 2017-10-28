1abdda00ef3573421dc135219c28e8e59efc84b9
URANO Ryoya <urano.works.mail@gmail.com>
2017-10-28 22:03:40 +0900

    不要なimportの削除

--- app/controllers/AdminController.scala
+++ app/controllers/AdminController.scala
@@ -1,13 +1,8 @@
 package controllers
 
 import javax.inject._
-import play.api.Play._
 import play.api.mvc._
-import play.api.data.Form
-import play.api.data.Forms._
-import play.api.i18n.Messages.Implicits._
 import play.api.db._
-import formModels._
 import databaseModels._
 
 @Singleton
