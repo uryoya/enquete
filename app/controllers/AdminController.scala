5fcbe7ba5c05745ce6678b7cd50f808d59ef623d
URANO Ryoya <urano.works.mail@gmail.com>
2017-10-28 17:45:47 +0900

    Controllerのスタブを作成

--- app/controllers/AdminController.scala
+++ app/controllers/AdminController.scala
@@ -12,5 +12,31 @@ import databaseModels._
 
 @Singleton
 class AdminController @Inject()(db: Database) extends Controller {
-  val userModel: Usermodel = new UserModel(db)
+  val userModel: UserModel = new UserModel(db)
+
+  def index = Action { implicit request =>
+    request.session.get("login").map { userId =>
+      val admin = userModel.getUser(userId.toInt)
+      if (admin.id == 0) {
+        Ok("Admin's index page")
+      } else {
+        Redirect(routes.EnqueteController.index())
+      }
+    }.getOrElse {
+      Redirect(routes.AuthorizeController.signin())
+    }
+  }
+
+  def authorizeUser(id: Int) = Action { implicit request =>
+    request.session.get("login").map { userId =>
+      val admin = userModel.getUser(userId.toInt)
+      if (admin.id == 0) {
+        Ok("Authorize the new user")
+      } else {
+        Redirect(routes.EnqueteController.index())
+      }
+    }.getOrElse {
+      Redirect(routes.AuthorizeController.signin())
+    }
+  }
 }
